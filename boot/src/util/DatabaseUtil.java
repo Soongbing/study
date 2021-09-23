7698f8e73824e12131db31747e6a5a3b185ddece
Soongbing <91145414+Soongbing@users.noreply.github.com>
2021-09-23 14:08:39 +0900

    Update DatabaseUtil.java

--- boot/src/util/DatabaseUtil.java
+++ boot/src/util/DatabaseUtil.java
@@ -9,7 +9,7 @@ public class DatabaseUtil {
 		try {
 			String dbURL = "jdbc:mysql://localhost:3306/LectureEvaluation";
 			String dbID = "root";
-			String dbPassword = "tndrmqldrm12";
+			String dbPassword = "비밀번호";
 			Class.forName("com.mysql.jdbc.Driver");
 			return DriverManager.getConnection(dbURL, dbID, dbPassword);
 		}catch(Exception e) {
